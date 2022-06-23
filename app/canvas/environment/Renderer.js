b9297cc12fafb2202c870069d27e150888f7a223
TKS31 <aokitsukasa31@gmail.com>
2022-06-24 06:19:13 +0900

    change: WebGLRendererを継承

--- app/canvas/environment/Renderer.js
+++ app/canvas/environment/Renderer.js
@@ -2,25 +2,21 @@ import { WebGLRenderer } from "three";
 import Size from "../../utils/Size";
 import Canvas from "../Canvas";
 
-export default class Renderer {
-  constructor() {
-    this.setInstance();
-  }
-
-  setInstance() {
-    this.instance = new WebGLRenderer({
-      canvas: document.getElementById('canvas'),
+export default class Renderer extends WebGLRenderer {
+  constructor({ canvas }) {
+    super({
+      canvas
     });
-    this.instance.setPixelRatio(Size.pixelRatio);
-    this.instance.setSize(Size.width, Size.height);
+    this.setPixelRatio(Size.pixelRatio);
+    this.setSize(Size.width, Size.height);
   }
 
   update() {
-    this.instance.render(Canvas.scene, Canvas.camera.instance);
+    this.render(Canvas.scene, Canvas.camera);
   }
 
   onResize() {
-    this.instance.setPixelRatio(Size.pixelRatio);
-    this.instance.setSize(Size.width, Size.height);
+    this.setPixelRatio(Size.pixelRatio);
+    this.setSize(Size.width, Size.height);
   }
 }
