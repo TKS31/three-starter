8fa1cd9b4643b46e28147db0b1deb887b39dff3a
roukara <aokitsukasa31@gmail.com>
2023-05-21 07:31:29 +0900

    refactor: query関数でdom要素を取得

--- src/scripts/components/Canvas.js
+++ src/scripts/components/Canvas.js
@@ -1,30 +1,28 @@
-import { $ } from "../helpers/dom";
+import { query } from "../helpers/query";
 import { getWindowSize } from "../helpers/getWindowSize";
 import { Plane } from '../webgl/objects/Plane';
 import { WebGL } from "../webgl/WebGL";
 
 export class Canvas {
   constructor() {
-    this.refs = {
-      wrapper: $('.canvas-wrapper')
-    };
-
+    this.wrapper = query('.canvas-wrapper');
+    
     const { width, height, dpr } = getWindowSize();
 
     this.webgl = new WebGL({ width, height, dpr });
     this.el = this.webgl.canvas;
 
-    this.refs.wrapper.appendChild(this.el);
+    this.wrapper.appendChild(this.el);
 
     const plane = new Plane();
     this.webgl.add(plane.mesh);
   }
 
-  raf() {
+  raf = () => {
     this.webgl.render();
   }
 
-  resize({ width, height, dpr }) {
+  resize = ({ width, height, dpr }) => {
     this.webgl.resize({ width, height, dpr });
   }
 }
